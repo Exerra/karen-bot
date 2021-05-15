947ca8219576bf184b0d94be3a2265cedf57a85e
Exerra <exerra@exerra.xyz>
2021-05-16 00:39:12 +0300

    ok maybe i shouldnt include corona gay cancer [skip ci]

--- commands/ban.js
+++ commands/ban.js
@@ -28,7 +28,7 @@ module.exports = {
 
         await member.send(`onmg AHahaHAHahaAHAHAHAHA im wheezing rn you got banned from ${msg.guild.name} (id: ${msg.guild.id}) for ${reason}. excuse me how the hell did u get banend?? aaaaaanyway... thats what you get you devil! REPEL THE DEMONS! UNBLOW!!! YOU ARE DESTROYED FOREVER!!! AND YOU WILL NEVER BE BACK!!!! thank you god... let it happen... cause it to happen`)
         await member.send('https://static.independent.co.uk/s3fs-public/thumbnails/image/2020/04/05/16/kenneth-copeland-blow-coronavirus.png?width=1200')
-        await member.send('oh and btw 5g causes corona gay cancer and reptiliioans are hacking our brrains from mars while probing our pets so like you should be saying thanks for getting educated 😒')
+        await member.send('oh and btw 5g causes corona cancer and reptiliioans are hacking our brrains from mars while probing our pets so like you should be saying thanks for getting educated 😒')
 
         // Time for public humiliation
         await member.ban({reason}).catch(error => msg.reply(`Sorry ${msg.author} I couldn't ban because of : ${error}`));
