4c0c4c93ef0202e23c8261ec2e1982d250a29c15
Exerra <exerra@exerra.xyz>
2021-06-07 00:17:44 +0300

    Typing indicator for song.js

--- commands/song.js
+++ commands/song.js
@@ -25,6 +25,8 @@ module.exports = {
         secret: process.env.SPOTIFY_SECRET
     });
 
+    msg.channel.startTyping()
+
     // Tries to search
     try {
         spotify
@@ -34,41 +36,36 @@ module.exports = {
                 if (response.tracks.items[0] == null) return msg.channel.send(`Error: No search results for \`${args[0]}\``)
                 // idk?
                 action.response = response;
-                // Sends a "looking up" message, then edits it to show the response stuff
-                msg.channel.send(`:compass: Looking up \`${args[0]}\``).then(async (msg) => {
-                    const embed = new Discord.MessageEmbed()
-                    embed.setTitle(response.tracks.items[0].name)
-                    embed.setURL(response.tracks.items[0].external_urls.spotify)
-                    embed.setThumbnail(response.tracks.items[0].album.images[0].url)
-                    embed.setAuthor('Spotify', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/240px-Spotify_logo_without_text.svg.png')
-                    embed.setColor(config.color)
-                    embed.addField('Monthly popularity', `${Math.trunc(response.tracks.items[0].popularity / 10)} / 10`)
-                    embed.addField('Album name', response.tracks.items[0].album.name)
-                    embed.addField('Album Type', response.tracks.items[0].album.album_type.capitalize())
-                    // thanks to @levichlev for making this thingy
-                    // i forgot about "for" loops (i dont really know why) when making this and was stumped on how to make it nicer if there are multiple artists :)
-                    var aname = 'Artist\'s name'
-                    if (response.tracks.items[0].album.artists.length != 1) {
-                        var a = [];
-                        for (i in response.tracks.items[0].album.artists) {
-                            a[i] = response.tracks.items[0].album.artists[i].name;
-                        }
-                        aname = 'Artist\'s names'
-                        embed.addField(aname, a.join('\n'))
-                    } else {
-                        embed.addField(aname, response.tracks.items[0].album.artists[0].name)
-                    }
-                    embed.addField('Release Date', response.tracks.items[0].album.release_date + '\n(Year-Month-Day)', true)
-                    embed.setTimestamp()
-                    return msg.edit('Here you go!').then(() => { msg.edit(embed) })
-                        .catch((err) => {
-                            console.error('Error occurred: ' + err);
-                        });
 
-                })
+                const embed = new Discord.MessageEmbed()
+                embed.setTitle(response.tracks.items[0].name)
+                embed.setURL(response.tracks.items[0].external_urls.spotify)
+                embed.setThumbnail(response.tracks.items[0].album.images[0].url)
+                embed.setAuthor('Spotify', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/240px-Spotify_logo_without_text.svg.png')
+                embed.setColor(config.color)
+                embed.addField('Monthly popularity', `${Math.trunc(response.tracks.items[0].popularity / 10)} / 10`)
+                embed.addField('Album name', response.tracks.items[0].album.name)
+                embed.addField('Album Type', response.tracks.items[0].album.album_type.capitalize())
+                // thanks to @levichlev for making this thingy
+                // i forgot about "for" loops (i dont really know why) when making this and was stumped on how to make it nicer if there are multiple artists :)
+                var aname = 'Artist\'s name'
+                if (response.tracks.items[0].album.artists.length != 1) {
+                    var a = [];
+                    for (i in response.tracks.items[0].album.artists) {
+                        a[i] = response.tracks.items[0].album.artists[i].name;
+                    }
+                    aname = 'Artist\'s names'
+                    embed.addField(aname, a.join('\n'))
+                } else {
+                    embed.addField(aname, response.tracks.items[0].album.artists[0].name)
+                }
+                embed.addField('Release Date', response.tracks.items[0].album.release_date + '\n(Year-Month-Day)', true)
+                embed.setTimestamp()
+                return msg.channel.send(embed), msg.channel.stopTyping()
             })
     } catch (err) {
         msg.channel.send(err)
+        msg.channel.stopTyping()
     }
   }
 }
