688a3ae53c1b27435dfed87442d7c932e3a47c3a
Exerra <exerra@exerra.xyz>
2021-06-11 21:16:55 +0300

    Improved code structure in bot.js 'statsTimeout()'

--- bot.js
+++ bot.js
@@ -353,27 +353,27 @@ client.once('ready', async () => {
 
     Promise.all(promises)
     .then(results => {
-        const totalGuilds = results[0].reduce((prev, guildCount) => prev + guildCount, 0);
-        const totalMembers = results[1].reduce((prev, memberCount) => prev + memberCount, 0);
+      const totalGuilds = results[0].reduce((prev, guildCount) => prev + guildCount, 0);
+      const totalMembers = results[1].reduce((prev, memberCount) => prev + memberCount, 0);
 
-        axios({
-          "method": "POST",
-          "url": `${process.env.API_SERVER}/karen/stats/`,
-          "headers": {
-            "Authorization": process.env.AUTH_B64,
-            "Content-Type": "application/json; charset=utf-8",
-            'User-Agent': process.env.AUTH_USERAGENT
-          },
-          "auth": {
-            "username": process.env.AUTH_USER,
-            "password": process.env.AUTH_PASS
-          },
-          "data": {
-            "servercount": `${totalGuilds}`,
-            "users": `${totalMembers}`,
-            "DiscordJS": `${Discord.version}`
-          }
-        })
+      axios({
+        "method": "POST",
+        "url": `${process.env.API_SERVER}/karen/stats/`,
+        "headers": {
+          "Authorization": process.env.AUTH_B64,
+          "Content-Type": "application/json; charset=utf-8",
+          'User-Agent': process.env.AUTH_USERAGENT
+        },
+        "auth": {
+          "username": process.env.AUTH_USER,
+          "password": process.env.AUTH_PASS
+        },
+        "data": {
+          "servercount": `${totalGuilds}`,
+          "users": `${totalMembers}`,
+          "DiscordJS": `${Discord.version}`
+        }
+      })
     })
     .catch(console.error);
   }
