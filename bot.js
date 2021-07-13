9c3a08fc8da8108a1f6c00b50a027edafc82ec5f
Exerra <exerra@exerra.xyz>
2021-07-13 12:11:56 +0300

    Brew search command

--- bot.js
+++ bot.js
@@ -537,6 +537,17 @@ client.on('message', async msg => {
     return (url.match(p)) ? true : false ;
   }
 
+  // Args thing for the {{thing}}
+  const foundargs = msg.content.split(" ")
+  var found = "",          // an array to collect the strings that are found
+    rxp = /{{([^}]+)}}/g,
+    str = foundargs[0],
+    curMatch;
+
+  while( curMatch = rxp.exec( str ) ) {
+    found = curMatch[1];
+  }
+
   const spotargs = msg.content.split(" ");
 
   if (matchSpotifyTrackUrl(spotargs[0])) {
@@ -637,6 +648,111 @@ client.on('message', async msg => {
         msg.channel.send(embed)
 
       })
+  }
+
+  /*
+    TODO: Finish this brew search; Finish the brew search settings; Tidy up code; Remove brew.js
+  */
+  
+  // If it found something, continue
+  if (found !== "") {
+    // If the brewSearch setting is turned off, return
+    if (!settingsmap.get(msg.guild.id).brewSearch) return
+    // Call the API
+    axios.get(`https://formulae.brew.sh/api/formula/${found}.json`).then(res => {
+      let data = res.data
+      // Testing stuff, delyeet later
+      const embed = new Discord.MessageEmbed()
+        .setColor("FFA743")
+        .setTitle(data.name)
+        .setURL(`https://formulae.brew.sh/formula/${data.name}`)
+        .setThumbnail('https://brew.sh/assets/img/homebrew-256x256.png')
+        .setDescription(data.desc)
+        .setAuthor('brew.sh')
+        .addFields(
+          { name: `Stable version`, value: data.versions.stable, inline: true },
+          { name: "License", value: data.license, inline: true }
+        )
+        // Check if the bottle files are longer than 1
+        // Theyre needed to check compability
+        if (data.bottle.stable.files.length != 1) {
+          // Gets the keys in the API responses object
+          var compabilityItems = Object.keys(data.bottle.stable.files)
+          // Testing stuff
+
+          // Function to set compability item, makes the switch statement easier to type && less cluttered
+          const setCompabilityItem = (item, value) => {
+            compabilityItems[item] = value
+          }
+
+          // For each item in the compabilityItems array, check the value and make it more human
+          for (let i in compabilityItems) {
+            switch(compabilityItems[i]) {
+              case 'all':
+                setCompabilityItem(i, 'All macOS and Linux systems')
+                break;
+              case 'arm64_big_sur':
+                setCompabilityItem(i, 'Apple Silicon Big Sur')
+                break;
+              case 'big_sur':
+                setCompabilityItem(i, 'Intel Big Sur')
+                break;
+              case 'catalina':
+                setCompabilityItem(i, 'Intel Catalina')
+                break;
+              case 'mojave':
+                setCompabilityItem(i, 'Intel Mojave')
+                break;
+              case 'high_sierra':
+                setCompabilityItem(i, 'Intel High Sierra')
+                break;
+              case 'sierra':
+                setCompabilityItem(i, 'Intel Sierra')
+                break;
+              case 'el_capitan':
+                setCompabilityItem(i, 'Intel El Capitan')
+                break;
+              case 'yosemite':
+                setCompabilityItem(i, 'Intel Yosemite')
+                break;
+              case 'mavericks':
+                setCompabilityItem(i, 'Intel Mavericks')
+                break;
+              case 'x86_64_linux':
+                setCompabilityItem(i, 'x86_64 Linux')
+                break;
+            }
+          }
+          // Add the compability items in a field
+          embed.addField("Compability", compabilityItems.join('\n'))
+        } else {
+          let compabilityItems = Object.keys(data.bottle.stable.files)
+            embed.addField("Compability", compabilityItems[0])
+        }
+
+        // Checks if outdated, disabled and deprecated is true
+        // If it is true, then it adds the field, but if it isn't, it doesn't because it looks ugly
+        if (data.outdated == true) embed.addField('Outdated?', data.outdated.toString())
+        if (data.disabled == true) embed.addField('Disabled?', data.outdated.toString())
+        if (data.deprecated == true) {
+          embed.addField('Deprecated?', data.deprecated.toString())
+          embed.addField('Date of deprecation', data.deprecated_date, true)
+          embed.addField('Reason for deprecation', data.deprecated_reason, true)
+        }
+        // Total amount of downloads
+        embed
+          .addFields(
+            { name: "Downloads (30d)", value: data.analytics.install['30d'][Object.keys(data.analytics.install['30d'])[0]], inline: true },
+            { name: "Downloads (365d)", value: data.analytics.install['365d'][Object.keys(data.analytics.install['365d'])[0]], inline: true }
+          )
+          .setFooter(`Requested by ${msg.author.username} • Generated at ${data.generated_date}`, msg.author.avatarURL({ dynamic: true }))
+
+        // Ofc, send the message when we're done
+        msg.channel.send(embed)
+    }, error => {
+      // just return
+      return
+    })
   }
   // garbage code end
 
