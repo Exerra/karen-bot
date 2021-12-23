b100447eefecae90fe85ffeeb3a5aee06fe37c30
Exerra <exerra@exerra.xyz>
2021-12-23 13:46:31 +0200

    Various yasses [deploy]

--- commands/eval.js
+++ commands/eval.js
@@ -4,60 +4,60 @@ const evalLangList = require('../util/langList.js')
 const exec = require('child_process').execSync
 const hastebin = require("hastebin-gen");
 let embede,
-    result,
-    fail,
-    start,
-    argss,
-    lang
+	result,
+	fail,
+	start,
+	argss,
+	lang
 
 module.exports = {
-  name: 'eval',
-  description: 'Execute some code.',
-  type: 'Private',
-  permissionsLevel: 'Bot Owner',
-  async execute(client, msg, args) {
-    const app = require('../bot.js')
-    start = new Date()
-    // NOTE: Keep this as a fallback. Just in case
-    // argss = msg.content.replace(RegExp(`${msg.prefix}eval\\s+(\\n?)+`, `gi`), ``)
-    argss = msg.content.match(/```.*\s*.*\s*```/gs)
-    if(argss == null) return msg.channel.send(`Please provide a valid codeblock.`)
-    argss = argss[0].replace(/\s*```.*\s*/g, '')
-    lang = msg.content.match(/```.*\s*.+\s*```/gs)[0].match(/```.*/g) ?
-           msg.content.match(/```.*\s*.+\s*```/gs)[0].match(/```.*/g)[0].replace('```', '')
-           : 'js'
-    lang = evalLangList[lang] || {
-      extension: 'js',
-      command: {
-        type: 'eval'
-      }
-    }
-    try {
-      switch(lang.command.type) {
-      case 'eval':
-        result = inspect(eval(argss), { depth: 1 })
-        break
-      case 'exec':
-        msg.channel.send(':warning: **This may take some time**')
-        result = exec(lang.command.run.replace(/<code>/gi, argss).replace(/<ext>/gi, lang.extension)).toString()
-        console.log(result) // Just in case
-        break
-      default:
-        msg.channel.send(`:x: **No command type defined.** Skipping...`)
-      }
-    } catch(e) {
-      result = e
-      fail = true
-    }
-    if (result.length > 1024 && result.length < 80000) {
-      hastebin(result, { extension: lang.extension, url: 'https://paste.exerra.xyz'} ).then(haste => msg.channel.send(`Result was too big: ` + haste))
-    } else if(result.length > 80000) {
-      msg.channel.send(`I was going to send this in a hastebin, but the result is over 80,000 characters!`)
-    } else {
-      msg.channel.send(new Discord.MessageEmbed()
-        .addField(`\u200B`, `\`\`\`js\n${result}\`\`\``)
-        .setColor(fail ? `#ff0033` : `#8074d2`)
-        .setFooter(`${new Date() - start}ms`, msg.author.avatarURL()))
-    }
-  }
+	name: 'eval',
+	description: 'Execute some code.',
+	type: 'Private',
+	permissionsLevel: 'Bot Owner',
+	async execute(client, msg, args) {
+		const app = require('../bot.js')
+		start = new Date()
+		// NOTE: Keep this as a fallback. Just in case
+		// argss = msg.content.replace(RegExp(`${msg.prefix}eval\\s+(\\n?)+`, `gi`), ``)
+		argss = msg.content.match(/```.*\s*.*\s*```/gs)
+		if(argss == null) return msg.channel.send(`Please provide a valid codeblock.`)
+		argss = argss[0].replace(/\s*```.*\s*/g, '')
+		lang = msg.content.match(/```.*\s*.+\s*```/gs)[0].match(/```.*/g) ?
+			msg.content.match(/```.*\s*.+\s*```/gs)[0].match(/```.*/g)[0].replace('```', '')
+			: 'js'
+		lang = evalLangList[lang] || {
+			extension: 'js',
+			command: {
+				type: 'eval'
+			}
+		}
+		try {
+			switch(lang.command.type) {
+				case 'eval':
+					result = inspect(eval(argss), { depth: 1 })
+					break
+				case 'exec':
+					msg.channel.send(':warning: **This may take some time**')
+					result = exec(lang.command.run.replace(/<code>/gi, argss).replace(/<ext>/gi, lang.extension)).toString()
+					console.log(result) // Just in case
+					break
+				default:
+					msg.channel.send(`:x: **No command type defined.** Skipping...`)
+			}
+		} catch(e) {
+			result = e
+			fail = true
+		}
+		if (result.length > 1024 && result.length < 80000) {
+			hastebin(result, { extension: lang.extension, url: 'https://paste.exerra.xyz'} ).then(haste => msg.channel.send(`Result was too big: ` + haste))
+		} else if(result.length > 80000) {
+			msg.channel.send(`I was going to send this in a hastebin, but the result is over 80,000 characters!`)
+		} else {
+			msg.channel.send(new Discord.MessageEmbed()
+				.addField(`\u200B`, `\`\`\`js\n${result}\`\`\``)
+				.setColor(fail ? `#ff0033` : `#8074d2`)
+				.setFooter(`${new Date() - start}ms`, msg.author.avatarURL()))
+		}
+	}
 }
