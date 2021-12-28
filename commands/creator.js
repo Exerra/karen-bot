d7a6b94652fd2f50ead3172a98f22f46c0adf4b4
Exerra <exerra@exerra.xyz>
2021-12-28 19:17:08 +0200

    Creator command rework

--- commands/creator.js
+++ commands/creator.js
@@ -17,59 +17,64 @@ module.exports = {
 	description: 'Gets information about the creator of Karen Bot',
 	type: 'Search',
 	args: false,
-	disabled: true,
+	//disabled: true,
 	async execute(client, msg, args) {
 		const app = require('../bot.js');
 		let config = app.config;
-		// Fetches the API
-		const apiExerra = await (await fetch(`https://api.exerra.xyz/users/Exerra/stats.json`)).json()
 
-		// Constants
-		const exerraLanguageEmbed = new Discord.MessageEmbed()
-		const exerraServiceEmbed = new Discord.MessageEmbed()
-		const exerraExperienceEmbed = new Discord.MessageEmbed()
-
-		// Language Embed
-		exerraLanguageEmbed.setTitle(`${apiExerra.name} (Exerra)`)
-		exerraLanguageEmbed.setURL('https://exerra.xyz')
-		exerraLanguageEmbed.setColor(config.color)
-		exerraLanguageEmbed.setTimestamp()
-		exerraLanguageEmbed.setDescription(apiExerra.aboutme)
-		exerraLanguageEmbed.addField('Known Languages', apiExerra.skills)
-		exerraLanguageEmbed.setFooter(`With ❤️ from ${config.creator}`, config.logo)
-
-		// Services embed
-		exerraServiceEmbed.setTitle(`${apiExerra.name}'s Services`)
-		exerraServiceEmbed.setColor(config.color)
-		exerraServiceEmbed.setTimestamp()
-		exerraServiceEmbed.setFooter(`With ❤️ from ${config.creator}`, config.logo)
-		if (apiExerra.services.length != 1) {
-			apiExerra.services.forEach(element => {
-				exerraServiceEmbed.addField(`${element.name}`, element.description)
-			});
-		}
-		else {
-			exerraServiceEmbed.addField(apiExerra.services[0].name, apiExerra.services[0].description)
+		const empty = (str) => {
+			if (typeof str == 'undefined' || !str || str.length === 0 || str === "" || !/[^\s]/.test(str) || /^\s*$/.test(str) || str.replace(/\s/g,"") === "")
+				return true;
+			else
+				return false;
 		}
 
-		// Experience Embed
-		exerraExperienceEmbed.setTitle(`${apiExerra.name}'s Experience`)
-		exerraExperienceEmbed.setColor(config.color)
-		exerraExperienceEmbed.setTimestamp()
-		exerraExperienceEmbed.setFooter(`With ❤️ from ${config.creator}`, config.logo)
-		if (apiExerra.experience.length != 1) {
-			apiExerra.experience.forEach(element => {
-				console.log(element)
-				exerraExperienceEmbed.addField(`${element.year} - ${element.name}`, element.description)
-			});
-		}
-		else {
-			exerraExperienceEmbed.addField(`${apiExerra.experience[0].year} - ${apiExerra.experience[0].name}`, apiExerra.experience[0].description)
+
+		// Fetches from the API
+		const exerra = await (await fetch(`${process.env.API_SERVER}/karen/profile?id=391878815263096833&fetchUser=true`)).json()
+
+		const embed = new Discord.MessageEmbed()
+
+		// Variable to check how much fields the top line has
+		// Useful for adding spacers
+		let topLineFieldAmount = 0
+
+		embed.setTitle(`${exerra.username}'s profile`);
+		embed.setThumbnail(exerra.avatar.url.webp);
+		embed.addField("Description", exerra.profile.description)
+
+		if (!empty(exerra.profile.website)) embed.addField('Website', `[${exerra.profile.website.replace(/(^\w+:|^)\/\//, '')}](${exerra.profile.website} '${exerra.username}'s website')`, true); topLineFieldAmount++
+		if (!empty(exerra.profile.email)) embed.addField('Email', `[${exerra.profile.email}](mailto:${exerra.profile.email})`, true); topLineFieldAmount++
+		if (!empty(exerra.profile.twitter)) embed.addField('Twitter', `[@${exerra.profile.twitter}](https://twitter.com/${exerra.profile.twitter} '${exerra.username}'s twitter')`, true); topLineFieldAmount++
+
+		// Switch statement to determine how much spacers to use
+		switch (topLineFieldAmount) {
+			case 0:
+				break;
+			case 1:
+				embed.addField('\u200B', '\u200B', true)
+				embed.addField('\u200B', '\u200B', true)
+				break;
+			case 2:
+				embed.addField('\u200B', '\u200B', true)
+				break;
+			case 3:
+				break;
 		}
 
-		// Sends embeds
-		msg.channel.send(exerraLanguageEmbed)
-		msg.channel.send(exerraServiceEmbed)
-		msg.channel.send(exerraExperienceEmbed)
+		embed.addField(`Birthday`, exerra.profile.birthday)
+
+		embed.addField("Pronouns", exerra.profile.pronouns, true)
+		embed.addField("Gender", exerra.profile.gender, true)
+		embed.addField("Country", exerra.profile.country, true)
+
+		embed.addField("Languages", exerra.profile.languages, true);
+		embed.addField('\u200B', '\u200B', true)
+
+		exerra.accentColor != null ? embed.setColor(exerra.accentColor) : embed.setColor(config.color)
+
+		embed.setAuthor("Karen Bot developer", "https://cdn.exerra.xyz/png/discord/verified-bot-developer.png")
+
+		msg.channel.send(embed)
 	}
 }
