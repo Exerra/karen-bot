6018f2cbdfd4d2abeb47ca8481abb630a6ceac87
Exerra <exerra@exerra.xyz>
2022-01-02 15:24:31 +0200

    rip bozo [deploy]

--- bot.js
+++ bot.js
@@ -14,14 +14,14 @@ require('./modules/functions.js')(client)
 var NSFAI = require('nsfai');
 require('dotenv').config()
 const Spotify = require('node-spotify-api')
-const { serverFunc } = require('./modules/serverFunc.js')
-const { matchRegex } = require('./modules/regex.js')
-const { autoEmbeds } = require('./modules/autoEmbeds.js')
- 
+const {serverFunc} = require('./modules/serverFunc.js')
+const {matchRegex} = require('./modules/regex.js')
+const {autoEmbeds} = require('./modules/autoEmbeds.js')
+
 var nsfai = new NSFAI(process.env.NSFAI_KEY);
 
 // commands stuff
-let commands = {}; 
+let commands = {};
 let slashCommands = {}
 
 // mobile status
@@ -31,18 +31,18 @@ const Constants = require('./node_modules/discord.js/src/util/Constants.js') // 
 
 // winston logger (hopefully gonna replace console.log with this)
 const winston = require('winston');
-const { log } = require('./modules/log');
+const {log} = require('./modules/log');
 const logger = winston.createLogger({
 	transports: [
 		new winston.transports.Console(),
-		new winston.transports.File({ filename: 'log' }),
+		new winston.transports.File({filename: 'log'}),
 	],
 	format: winston.format.printf(log => `[${log.level.toUpperCase()}] - ${log.message}`),
 });
 
 // Permissions
 client.levelCache = {}
-for(let i=0;i<client.config.permLevels.length;i++) {
+for (let i = 0; i < client.config.permLevels.length; i++) {
 	const thisLevel = client.config.permLevels[i]
 	client.levelCache[thisLevel.name] = thisLevel.level
 }
@@ -54,17 +54,17 @@ client.failedEvents = []
 let cmdAlpha = {}
 let commandAmount = 0
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
-for(const file of commandFiles) {
-	try{
+for (const file of commandFiles) {
+	try {
 		const command = require(`./commands/${file}`)
-		if(!cmdAlpha[command.name.charAt(0)]) {
+		if (!cmdAlpha[command.name.charAt(0)]) {
 			cmdAlpha[command.name.charAt(0)] = true
 		}
 		client.commands.set(command.name, command)
-        commandAmount++
-	} catch(e) {
+		commandAmount++
+	} catch (e) {
 		client.failedCommands.push([file.split('.')[0], e.toString()])
-        log(`Error while loading command: ${file.split('.')[0]}: ${e}`, "error")
+		log(`Error while loading command: ${file.split('.')[0]}: ${e}`, "error")
 	}
 }
 log(`${chalk.magenta('[Karen Bot]')} ${chalk.yellow(`[Command]`)} ${chalk.white('[Load]')} Loaded ${commandAmount} commands`)
@@ -77,17 +77,17 @@ client.slashFailedEvents = []
 let slashCmdAlpha = {}
 let slashCommandAmount = 0
 const slashCommandFiles = fs.readdirSync('./slashcommands').filter(file => file.endsWith('.js'))
-for(const file of slashCommandFiles) {
-	try{
+for (const file of slashCommandFiles) {
+	try {
 		const slashCommand = require(`./slashcommands/${file}`)
-		if(!slashCmdAlpha[slashCommand.name.charAt(0)]) {
+		if (!slashCmdAlpha[slashCommand.name.charAt(0)]) {
 			slashCmdAlpha[slashCommand.name.charAt(0)] = true
 		}
 		client.slashcommands.set(slashCommand.name, slashCommand)
-        slashCommandAmount++
-	} catch(e) {
+		slashCommandAmount++
+	} catch (e) {
 		client.slashFailedCommands.push([file.split('.')[0], e.toString()])
-        log(`Error while loading slash command: ${file.split('.')[0]}: ${e}`, "error")
+		log(`Error while loading slash command: ${file.split('.')[0]}: ${e}`, "error")
 	}
 }
 log(`${chalk.magenta('[Karen Bot]')} ${chalk.yellow(`[SlashCommand]`)} ${chalk.white('[Load]')} Loaded ${slashCommandAmount} commands`)
@@ -96,18 +96,18 @@ log(`${chalk.magenta('[Karen Bot]')} ${chalk.yellow(`[SlashCommand]`)} ${chalk.w
 let evAlpha = {}
 let events = 0
 const eventFiles = fs.readdirSync('events/').filter(file => file.endsWith('.js'))
-for(const ev of eventFiles) {
+for (const ev of eventFiles) {
 	const eventName = ev.split('.')[0]
 	try {
-		if(!evAlpha[eventName.charAt(0)]) {
+		if (!evAlpha[eventName.charAt(0)]) {
 			evAlpha[eventName.charAt(0)] = true
 		}
 		const evx = require(`./events/${ev}`)
 		client.on(eventName, evx.bind(null, client))
-        events++
-	} catch(e) {
+		events++
+	} catch (e) {
 		client.failedEvents.push([eventName, e.toString()])
-        log(`Error while loading event: ${eventName}: ${e}`, "error")
+		log(`Error while loading event: ${eventName}: ${e}`, "error")
 	}
 }
 log(`${chalk.magenta('[Karen Bot]')} ${chalk.yellow('[Event]')} ${chalk.white("[Load]")} Loaded ${events} events`)
@@ -122,17 +122,17 @@ axios({
 	"url": `${process.env.API_SERVER}/karen/settings/map/`,
 	"headers": {
 		"Authorization": process.env.AUTH_B64,
-        'User-Agent': process.env.AUTH_USERAGENT
+		'User-Agent': process.env.AUTH_USERAGENT
 	},
 	"auth": {
 		"username": process.env.AUTH_USER,
 		"password": process.env.AUTH_PASS
 	}
 }).then(res => {
-    settingsmap = new Map(res.data); // skipcq: JS-0128
-    console.log(chalk.magenta('[Karen Bot]'), chalk.yellow(`[Settings]`), chalk.white('[Load]'), `Loaded guild settings`)
+	settingsmap = new Map(res.data); // skipcq: JS-0128
+	console.log(chalk.magenta('[Karen Bot]'), chalk.yellow(`[Settings]`), chalk.white('[Load]'), `Loaded guild settings`)
 }).catch(err => {
-    console.log(chalk.magenta('[Karen Bot]'), chalk.yellow(`[Settings]`), chalk.red('[Warn]'), `Failed to load guild settings`)
+	console.log(chalk.magenta('[Karen Bot]'), chalk.yellow(`[Settings]`), chalk.red('[Warn]'), `Failed to load guild settings`)
 })
 
 // Gets guildProfile map //! Levels are not yet released online so hush hush
@@ -141,163 +141,228 @@ axios({
 	"url": `${process.env.API_SERVER}/karen/guildProfile/map/`,
 	"headers": {
 		"Authorization": process.env.AUTH_B64,
-        'User-Agent': process.env.AUTH_USERAGENT
+		'User-Agent': process.env.AUTH_USERAGENT
 	},
 	"auth": {
 		"username": process.env.AUTH_USER,
 		"password": process.env.AUTH_PASS
 	}
 }).then(res => {
-    guildProfile = new Map(res.data);
+	guildProfile = new Map(res.data);
 })
 
 client.on('message', async msg => {
 
-  if(msg.content.includes(process.env.DISCORD_TOKEN)) return msg.delete()
-  if(msg.author.bot || msg.webhookID || !msg.author) return
+	if (msg.content.includes(process.env.DISCORD_TOKEN)) return msg.delete()
+	if (msg.author.bot || msg.webhookID || !msg.author) return
 
-    // el troll
-    for (let i in config.trolls) {
-        let troll = config.trolls[i]
+	// el troll
+	for (let i in config.trolls) {
+		let troll = config.trolls[i]
 
-        if (msg.content.includes(troll.content)) {
-            if (troll.id == "") msg.lineReply(troll.reply)
-            else if (msg.author.id == troll.id) msg.lineReply(troll.reply)
-        }
-    }
+		if (msg.content.includes(troll.content)) {
+			if (troll.id == "") msg.lineReply(troll.reply)
+			else if (msg.author.id == troll.id) msg.lineReply(troll.reply)
+		}
+	}
 
-  // Tbh idk why I did this, I wrote this at like 04:00
-  try {
+	// Tbh idk why I did this, I wrote this at like 04:00
+	try {
 		let guildPrefixLet = settingsmap.get(msg.guild.id).guildPrefix // skipcq: JS-0128
 		if (settingsmap.get(msg.guild.id).brewSearch == undefined) {
 			await settingsmap.set(msg.guild.id, {...settingsmap.get(msg.guild.id), brewSearch: false})
 			serverFunc.updateGuildSettings(settingsmap)
 		}
-  } catch (err) {
-	if (msg.guild.id == '793297057216856085') console.log(1.2)
-	serverFunc.createGuildSettings(msg.guild.id)
-  }
+	} catch (err) {
+		if (msg.guild.id == '793297057216856085') console.log(1.2)
+		serverFunc.createGuildSettings(msg.guild.id)
+	}
 
-  autoEmbeds(msg)
+	autoEmbeds(msg)
 
-  if (msg.attachments.size > 0) {
-    if (msg.attachments.every(attachIsImage)){
-      //something
-    }
-  }
-          
-  function attachIsImage(msgAttach) {
-    var url = msgAttach.url;
-    function handleResult(result) {
-      if (result.sfw) {
-        return
-      } else {
-        msg.reply('NSFW images are not allowed in SFW channels!')
-        msg.delete()
-      }
-    }
-    
-    function handleError(error) {
-      console.error('hm');
-    }
-    if (settingsmap.get(msg.guild.id).antiNSFW == false) return;
-    if (msg.channel.nsfw) return;
-    nsfai.predict(url).then(handleResult).catch(handleError);
-  }
+	if (msg.attachments.size > 0) {
+		if (msg.attachments.every(attachIsImage)) {
+			//something
+		}
+	}
 
-  const executeCommand = async (prefix) => {
-    var args = msg.content.slice(prefix.length).split(/\s+/)
-    
-    var commandName = args.shift().toLowerCase()
-    let command = client.commands.get(commandName)
-        || client.commands.find(c => c.aliases && c.aliases.includes(commandName))
+	function attachIsImage(msgAttach) {
+		var url = msgAttach.url;
 
-    if(!command) return;
-
-    const level = client.permLevel(msg)
-    if(level < client.levelCache[command.permissionsLevel || "User"]) {
-      return msg.channel.send(`Shut up, you're not my mom 😒🙄`)
-    }
-
-    msg.author.permLevel = level
-    msg.member.user.permLevel = level
-
-    try {
-
-    	if (command.disabled) {
-    		return msg.lineReply("Its ✨ disabled ✨\n\n\n\n\n\n\n..just like you apparently lMFAOOO")
+		function handleResult(result) {
+			if (result.sfw) {
+				return
+			} else {
+				msg.reply('NSFW images are not allowed in SFW channels!')
+				msg.delete()
+			}
 		}
 
-      if(command.args && !args.length) {
-        let reply = `Where are the arguments?? Explain yourself, WHERE IN THE FUCK DO YOU SEE ARGUMENTS!?!?!?`
+		function handleError(error) {
+			console.error('hm');
+		}
 
-        if(command.usage) {
-          reply += `\nProper Usage: \`${prefix}${command.name} ${command.usage.replace('shard_count', 4)}\``
-        }
+		if (settingsmap.get(msg.guild.id).antiNSFW == false) return;
+		if (msg.channel.nsfw) return;
+		nsfai.predict(url).then(handleResult).catch(handleError);
+	}
 
-        if(command.example) {
-          reply += `\n\`${prefix}${command.name} ${command.example.replace('shard_count', 3)}\``
-        }
-        return msg.channel.send(reply)
-      }
+	const executeCommand = async (prefix) => {
+		var args = msg.content.slice(prefix.length).split(/\s+/)
 
-      if(command.nsfw && !msg.channel.nsfw) {
-        if(!msg.channel.name.includes('nsfw')) return msg.channel.send(`Ugh this is an NSFW command, go to an NSFW channel 🙄`)
-      }
+		var commandName = args.shift().toLowerCase()
+		let command = client.commands.get(commandName)
+			|| client.commands.find(c => c.aliases && c.aliases.includes(commandName))
 
-      msg.channel.startTyping()
-      await command.execute(client, msg, args)
-      msg.channel.stopTyping()
-    } catch (error) {
-        console.error(error)
-        log(`"${msg}" by ${msg.author.id} - ${error}`, "error", true)
+		if (!command) return;
 
-      axios.post(process.env.REGULAR_WEBHOOK, {
-        "content": `"${msg}" by ${msg.author.id} - ${error}`,
-        "embeds": null,
-        "username": 'Karen Bot Error',
-        "avatar_url": "https://karen.exerra.xyz/assets/BotLogoNoOutline.png"
-      })
-      msg.reply('there was an error trying to execute that command.')
-      msg.channel.stopTyping()
-    }
-  }
+		const level = client.permLevel(msg)
+		if (level < client.levelCache[command.permissionsLevel || "User"]) {
+			return msg.channel.send(`Shut up, you're not my mom 😒🙄`)
+		}
 
-  if (msg.content.toLowerCase().startsWith(config.prefix)) executeCommand(config.prefix)
-  else if (settingsmap.get(msg.guild.id).guildPrefix !== "") {
-    if (msg.content.toLowerCase().startsWith(settingsmap.get(msg.guild.id).guildPrefix)) executeCommand(settingsmap.get(msg.guild.id).guildPrefix)
-  }
+		msg.author.permLevel = level
+		msg.member.user.permLevel = level
 
-  var message = msg.content.toLowerCase();
-  config.badwords.forEach(function(value) {
-    if(msg.content.toLowerCase() == value) {
-      if (msg.author.permLevel >= 4) return;
-      if (!settingsmap.get(msg.guild.id).swearProtectionEnabled) return;
-      msg.delete();
-      msg.reply(config.swearreply.randomize());
-    }
-  });
+		try {
 
-  // Literally the first lines of code in this project (back in 2019 or something). I find it funny how this has sorta became my "author check"
-  if(message === '> >run ping') {
-    msg.channel.send(`**Running Ping.exe...**`).then((msg)=> {
-      setTimeout(() => {
-        msg.edit('**Running Ping.exe...**\n**Found subroutine named "Ping Pong"**').then((msg)=> {
-          setTimeout(() => {
-            msg.edit('**Running Ping.exe...**\n**Found subroutine named "Ping Pong"**\n> >run Ping Pong').then((msg)=> {
-              setTimeout(() => {
-                msg.edit('**Running Ping.exe...**\n**Found subroutine named "Ping Pong"**\n> >run Ping Pong \n**Running Ping Pong subroutine**').then((msg)=> {
-                  setTimeout(() => {
-                      msg.edit('**Running Ping.exe...**\n**Found subroutine named "Ping Pong"**\n> >run Ping Pong \n**Running Ping Pong subroutine**\n**Error:** Subroutine corrupted, cancelling.');
-                  }, 2000)
-                });
-              }, 2000)
-            });
-          }, 2000)
-        });
-      }, 2000)
-    });
-  }
+			if (command.disabled) {
+				return msg.lineReply("Its ✨ disabled ✨\n\n\n\n\n\n\n..just like you apparently lMFAOOO")
+			}
+
+			if (command.args && !args.length) {
+				let reply = `Where are the arguments?? Explain yourself, WHERE IN THE FUCK DO YOU SEE ARGUMENTS!?!?!?`
+
+				if (command.usage) {
+					reply += `\nProper Usage: \`${prefix}${command.name} ${command.usage.replace('shard_count', 4)}\``
+				}
+
+				if (command.example) {
+					reply += `\n\`${prefix}${command.name} ${command.example.replace('shard_count', 3)}\``
+				}
+				return msg.channel.send(reply)
+			}
+
+			if (command.nsfw && !msg.channel.nsfw) {
+				if (!msg.channel.name.includes('nsfw')) return msg.channel.send(`Ugh this is an NSFW command, go to an NSFW channel 🙄`)
+			}
+
+			msg.channel.startTyping()
+			await command.execute(client, msg, args)
+			msg.channel.stopTyping()
+		} catch (error) {
+			console.error(error)
+			log(`"${msg}" by ${msg.author.id} - ${error}`, "error", true)
+
+			axios.post(process.env.REGULAR_WEBHOOK, {
+				"content": `"${msg}" by ${msg.author.id} - ${error}`,
+				"embeds": null,
+				"username": 'Karen Bot Error',
+				"avatar_url": "https://karen.exerra.xyz/assets/BotLogoNoOutline.png"
+			})
+			msg.reply('there was an error trying to execute that command.')
+			msg.channel.stopTyping()
+		}
+	}
+
+	const annoyCookies = () => {
+
+		if (msg.author.id != "817581327095693315") return
+
+		let args = msg.content.split(/\s+/)
+
+		for (let arg of args) {
+			if (arg.isURL()) {
+				let index = args.indexOf(arg);
+				if (index > -1) {
+					args.splice(index, 1);
+				}
+			}
+		}
+
+		if (args.length == 0) return
+
+		var options = {
+			method: 'POST',
+			url: 'https://jspell-checker.p.rapidapi.com/check',
+			headers: {
+				'content-type': 'application/json',
+				'x-rapidapi-host': 'jspell-checker.p.rapidapi.com',
+				'x-rapidapi-key': process.env.RAPIDAPI_KEY
+			},
+			data: {
+				language: 'enUS',
+				fieldvalues: args,
+				config: {
+					forceUpperCase: false,
+					ignoreIrregularCaps: false,
+					ignoreFirstCaps: true,
+					ignoreNumbers: true,
+					ignoreUpper: false,
+					ignoreDouble: false,
+					ignoreWordsWithNumbers: true
+				}
+			}
+		};
+
+		axios.request(options).then((response) => {
+			let data = response.data
+
+			if (data.spellingErrorCount === 0) return
+
+			let sentence = ""
+
+			for (let element of data.elements) {
+
+				if (element.errors.length == 0) {
+					sentence += `${args[element.id]} `
+					continue
+				}
+
+				sentence += `~~${args[element.id]}~~ ${element.errors[0].suggestions[0]} `
+			}
+
+			msg.lineReply(sentence)
+		})
+	}
+
+	if (msg.content.toLowerCase().startsWith(config.prefix)) executeCommand(config.prefix)
+	else if (settingsmap.get(msg.guild.id).guildPrefix !== "") {
+		if (msg.content.toLowerCase().startsWith(settingsmap.get(msg.guild.id).guildPrefix)) executeCommand(settingsmap.get(msg.guild.id).guildPrefix)
+	} else {
+		annoyCookies()
+	}
+
+	var message = msg.content.toLowerCase();
+	config.badwords.forEach(function (value) {
+		if (msg.content.toLowerCase() == value) {
+			if (msg.author.permLevel >= 4) return;
+			if (!settingsmap.get(msg.guild.id).swearProtectionEnabled) return;
+			msg.delete();
+			msg.reply(config.swearreply.randomize());
+		}
+	});
+
+	// Literally the first lines of code in this project (back in 2019 or something). I find it funny how this has sorta became my "author check"
+	if (message === '> >run ping') {
+		msg.channel.send(`**Running Ping.exe...**`).then((msg) => {
+			setTimeout(() => {
+				msg.edit('**Running Ping.exe...**\n**Found subroutine named "Ping Pong"**').then((msg) => {
+					setTimeout(() => {
+						msg.edit('**Running Ping.exe...**\n**Found subroutine named "Ping Pong"**\n> >run Ping Pong').then((msg) => {
+							setTimeout(() => {
+								msg.edit('**Running Ping.exe...**\n**Found subroutine named "Ping Pong"**\n> >run Ping Pong \n**Running Ping Pong subroutine**').then((msg) => {
+									setTimeout(() => {
+										msg.edit('**Running Ping.exe...**\n**Found subroutine named "Ping Pong"**\n> >run Ping Pong \n**Running Ping Pong subroutine**\n**Error:** Subroutine corrupted, cancelling.');
+									}, 2000)
+								});
+							}, 2000)
+						});
+					}, 2000)
+				});
+			}, 2000)
+		});
+	}
 });
 
 
--- modules/functions.js
+++ modules/functions.js
@@ -45,6 +45,12 @@ module.exports = client => {
             return false;
     }
 
+    String.prototype.isURL = function() {
+        let urlRegex = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
+        let url = new RegExp(urlRegex, 'i');
+        return this.length < 2083 && url.test(this);
+    }
+
     client.code = (lang, contents) => {
         return `${'```'}${lang}\n${contents}${'\n```'}`
     }
